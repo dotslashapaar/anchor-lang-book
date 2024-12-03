c63e32c8764ff3981f1866c7df10b069df298fec
Apaar Saini <apaarsaini007@gmail.com>
2024-12-03 09:17:29 +0530

    chapter 3

---
+++ 03-tic-tac-toe/programs/tic-tac-toe/src/lib.rs
@@ -0,0 +1,23 @@
+use anchor_lang::prelude::*;
+use instructions::*;
+use state::game::Tile;
+
+pub mod errors;
+pub mod instructions;
+pub mod state;
+
+// this key needs to be changed to whatever public key is returned by "anchor keys list"
+declare_id!("Gwr7Pf7123ESnbqpzgGefNpKJSgD38uH77r47DsK8DQF");
+
+#[program]
+pub mod tic_tac_toe {
+    use super::*;
+
+    pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
+        instructions::setup_game::setup_game(ctx, player_two)
+    }
+
+    pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
+        instructions::play::play(ctx, tile)
+    }
+}
